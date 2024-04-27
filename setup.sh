42de7dd08ea8a4c67f1ee1d85989b1b053ded474
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-04-27 07:37:56 +0200

    Cleanup comment
    we start from a git checkout, so files are already present

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@ apt -yy update
 apt -y install wget libgnutls30 ca-certificates rpm qemu-utils libwww-perl
 ./customsetup.sh
 
-# fetch perl script
-#TODO
-
 chmod a+x setup.pl
 # run perl script
 ./setup.pl
